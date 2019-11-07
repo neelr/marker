162a152b429428c3a40f9d17d82f439d6935750e
Neel Redkar <neel.redkar@outlook.com>
2019-11-06 18:49:09 -0800

    add github

---
+++ airtable.js
@@ -0,0 +1,80 @@
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base(process.env.AIRTABLE_BASE);
+var marked = require("marked");
+module.exports = class short {
+    constructor(id, pass, url, markdown,res) {
+        if (!id) {return null};
+        this.id = id;
+        this.passwd = pass;
+        this.url = url;
+        this.markdown = markdown;
+        let there = false;
+        let rec = "";
+        base('shorts').select({
+            maxRecords: 3,
+            view: "Grid view"
+        }).eachPage( (records, fetchNextPage) => {
+            records.forEach((record) => {
+                if (record.get("ID") == this.id) {
+                    there = true;
+                    rec = record;
+                }
+            });
+            fetchNextPage();
+        }, () => {
+            if (!there) {
+                this.verified = true;
+                base('shorts').create([
+                    {
+                        "fields": {
+                            "ID": this.id,
+                            "content": this.markdown,
+                            "pass": this.passwd,
+                            "uri": this.url
+                        }
+                    }],() => res.redirect("/"+this.id));
+            } else if (rec.get("pass") == this.passwd) {
+                this.verified = true;
+                base('shorts').update([
+                    {
+                      "id": rec.getId(),
+                      "fields": {
+                        "ID": this.id,
+                        "content": this.markdown,
+                        "pass": this.passwd,
+                        "uri":this.url
+                      }
+                    }], () => res.redirect("/"+this.id));
+            } else {
+                res.send("This record is taken and your password did not match");
+            }
+        });
+        
+    }
+    retrieve (id,callback) {
+        var triggered = false;
+        base('shorts').select({
+            maxRecords: 3,
+            view: "Grid view"
+        }).eachPage((records, fetchNextPage) => {
+            records.forEach((record) => {
+                if (record.get("ID") == id) {
+                    this.id = id;
+                    this.markdown = record.get("content");
+                    this.passwd = record.get("pass");
+                    this.url = record.get("uri");
+                    triggered = true;
+                }
+            });
+            fetchNextPage();
+        },() => {
+            callback(this,triggered);
+        });
+    }
+    render () {
+        return marked(this.markdown);
+    }
+    isVerified () {
+        return this.verified;
+    }
+}
---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+var express = require("express");
+var short = require("./airtable");
+var fs = require("fs");
+const app = express();
+app.use("/static",express.static(__dirname+"/public"))
+app.use(express.urlencoded());
+var TEMPLATE = fs.readFileSync("./public/template.html","utf8");
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/views/index.html")
+})
+app.post("/create",(req,res) => {
+    new short(req.body.id,req.body.password,req.body.url,req.body.markdown,res);
+})
+
+app.get("/:id",(req,res)=> {
+    var rec = new short();
+    rec.retrieve(req.params.id,(record,trigg) => {
+        if (trigg) {
+            if (record.markdown != undefined && record.markdown != "") {
+                let buff = TEMPLATE.replace("%title%",record.id);
+                res.send(buff.replace("%content%",record.render()));
+            } else {
+                res.redirect(record.url)
+            }
+        } else {
+            res.send("Record not found.")
+        }
+    });
+});
+
+app.listen(3000,()=> console.log('Markup is a runnin on port 3000!'));
